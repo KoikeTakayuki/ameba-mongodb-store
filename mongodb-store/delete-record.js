e4101717148684d992f6d8c1a889c88df77f6d9f
KoikeTakayuki <tekasax@gmail.com>
2017-04-23 01:36:08 +0900

    Implement delete

--- mongodb-store/delete-record.js
+++ mongodb-store/delete-record.js
@@ -1,1 +1,27 @@
-module.exports = record => record;
+const ObjectID = require('mongodb').ObjectID;
+const getRootType = require('ameba-util').getRootType;
+
+module.exports = (connection) => {
+  function deleteRecord(record) {
+    if (!record._id) {
+      throw Error('given record is not stored and cannot be deleted');
+    }
+
+    const collectionName = getRootType(record.type).id;
+
+    return connection
+      .then(db => db.collection(collectionName))
+      .then(collection => new Promise((success, failure) => {
+        const deleteCondition = { _id: new ObjectID(record._id) };
+        collection.remove(deleteCondition, { single: true }, (e) => {
+          if (e) {
+            failure(e);
+          } else {
+            success(true);
+          }
+        });
+      }));
+  }
+
+  return deleteRecord;
+};
