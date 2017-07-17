ca35ee0ba1073191234f44c956008a9ea487d456
koiketakayuki <koike.takayuki0907@gmail.com>
2017-07-17 22:55:02 +0900

    Save list ype

--- src/save.js
+++ src/save.js
@@ -1,6 +1,10 @@
 const store = require('./store');
 
 const save = db => (recordType, record) => {
+  if (Array.isArray(record)) {
+    return Promise.all(record.map(r => save(db)(recordType, r)));
+  }
+
   if (record._id) {
     return store(db).update(recordType, record).then(() => ({ _id: record._id }));
   }
